f0b6f63bee9e14b7130a6090faed0ca9a5ffef88
RPVanarx <roman.plynko@gmail.com>
2019-02-03 13:44:49 +0200

    snake case update

--- db/user.js
+++ db/user.js
@@ -3,7 +3,7 @@ const user = require('../dbconnect');
 async function create(platformId, userType, userName, latitude, longitude) {
     try {
         await user.query(
-            'INSERT INTO users VALUES(DEFAULT, $1, $2, $3, (point($4, $5)), 0, true) ON CONFLICT (platformId, platformType) DO UPDATE SET location = (point($4, $5))',
+            'INSERT INTO users VALUES(DEFAULT, $1, $2, $3, (point($4, $5))) ON CONFLICT (platform_id, platform_type) DO UPDATE SET location = (point($4, $5))',
             [platformId, userType, userName, latitude, longitude],
         );
     } catch (error) {
@@ -14,7 +14,7 @@ async function create(platformId, userType, userName, latitude, longitude) {
 async function changeActivity(platformId, userType, value) {
     try {
         await user.query(
-            'UPDATE users SET isactive = $1 WHERE platformId = $2 AND platformType = $3',
+            'UPDATE users SET is_active = $1 WHERE platform_id = $2 AND platform_type = $3',
             [value, platformId, userType],
         );
     } catch (error) {
--- dbCreate.sql
+++ dbCreate.sql
@@ -1,24 +1,24 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    platformId VARCHAR(50) NOT NULL,
-    platformType VARCHAR(10) NOT NULL,
-    userName VARCHAR(30) NOT NULL,
+    platform_id VARCHAR(50) NOT NULL,
+    platform_type VARCHAR(10) NOT NULL,
+    user_name VARCHAR(30) NOT NULL,
     location POINT NOT NULL,
-    badRequestCount INT DEFAULT 0,
-    isActive BOOLEAN DEFAULT true,
-    UNIQUE (platformId, platformType)
+    bad_request_count INT DEFAULT 0,
+    is_active BOOLEAN DEFAULT true,
+    UNIQUE (platform_id, platform_type)
 );
 
 CREATE TABLE requests(
     id SERIAL PRIMARY KEY,
-    userId BIGINT,
-    requestType VARCHAR(10) NOT NULL, 
+    user_id BIGINT,
+    request_type VARCHAR(10) NOT NULL, 
     location POINT NOT NULL,
     photo VARCHAR(100) NOT NULL,
     message TEXT NOT NULL,
-    creationDate DATE NOT NULL,
-    isApproved BOOLEAN,
-    statusChangedBy BIGINT NOT NULL,
-    isActive BOOLEAN,
-    FOREIGN KEY (userId) REFERENCES users(id),
+    creation_date DATE NOT NULL,
+    is_approved BOOLEAN,
+    status_changed_by BIGINT,
+    is_active BOOLEAN,
+    FOREIGN KEY (user_id) REFERENCES users(id),
 );
