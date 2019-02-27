77b1fc51679b5bbe3bba62d7d3434688d9437f6a
Vitali Timolyanov <nf.ebox@gmail.com>
2019-02-27 18:27:05 +0200

    Add default port value for WEB_PORT env

--- src/config/index.js
+++ src/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
   MODERATOR_GROUP_ID: process.env.MODERATORSID,
 
   WEB_AUTH_MAX_AUTH_PERIOD: process.env.WEB_AUTH_MAX_AUTH_PERIOD,
-  WEB_PORT: process.env.WEB_PORT,
+  WEB_PORT: process.env.WEB_PORT || 3003,
 
   RADIUS: 1000,
   PLATFORM_TYPE_TELEGRAM: 'telegram',
