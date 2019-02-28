c793aef3b0bc15164f68bd049b9bf35690e1bfaf
Vitali Timolyanov <nf.ebox@gmail.com>
2019-02-28 03:36:11 +0200

    Add requests/list/ path and JSON error on invalid path requests/any

--- src/web-api/routes/requests.js
+++ src/web-api/routes/requests.js
@@ -1,14 +1,25 @@
+const path = require('path');
 const { WEB_API_V1_PREFIX } = require('../../config');
 
-const route = `${WEB_API_V1_PREFIX}/requests`;
+const REQUEST_SUFFIX = '/requests';
+const LIST_SUFFIX = '/list';
+
+const requestsRoute = path.join(WEB_API_V1_PREFIX, REQUEST_SUFFIX);
+const listRoute = path.join(requestsRoute, LIST_SUFFIX);
 
 function setResponse(ctx) {
-  const message = { message: '' };
+  const message = { error: '', message: '' };
   ctx.body = message;
 }
 
+function set404(ctx) {
+  ctx.body = { error: 'Not Found' };
+  ctx.status = 404;
+}
+
 module.exports = ({ router }) => {
-  router.get(route, async ctx => {
-    setResponse(ctx);
-  });
+  router.get(listRoute, async ctx => setResponse(ctx));
+
+  const startsWithRequestRoute = new RegExp(`^${requestsRoute}(/|$)`);
+  router.get(startsWithRequestRoute, async ctx => set404(ctx));
 };
