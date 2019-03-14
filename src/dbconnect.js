1eff8e197789b48396ccdc03b820f293642ca684
RPVanarx <roman.plynko@gmail.com>
2019-03-14 17:01:19 +0200

    db reconnect

--- src/dbconnect.js
+++ src/dbconnect.js
@@ -2,32 +2,37 @@ const { Client } = require('pg');
 const { db } = require('./config');
 const migrate = require('./migrate');
 
-const client = new Client({
-  user: db.user,
-  host: db.host,
-  database: db.database,
-  password: db.password,
-  port: db.port,
-});
-
-async function dbInit() {
-  await client.query(migrate.cube);
-  await client.query(migrate.earthdistance);
-  await client.query(migrate.createTableUser);
-  await client.query(migrate.createTableRequests);
-}
-
-const createConnection = async () => {
-  try {
-    await client.connect();
-    await dbInit();
-    console.log('connected to base');
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
+const connection = () => {
+  return new Promise(async res => {
+    let client;
+    async function dbInit() {
+      await client.query(migrate.cube);
+      await client.query(migrate.earthdistance);
+      await client.query(migrate.createTableUser);
+      await client.query(migrate.createTableRequests);
+    }
+    let retries = 5;
+    while (retries) {
+      try {
+        client = new Client({
+          user: db.user,
+          host: db.host,
+          database: db.database,
+          password: db.password,
+          port: db.port,
+        });
+        await client.connect();
+        await dbInit();
+        console.log('database connected');
+        break;
+      } catch (err) {
+        retries -= 1;
+        console.log(`retries left ${retries} ${err}`);
+        await new Promise(res => setTimeout(res, 3000));
+      }
+    }
+    res(client);
+  });
 };
 
-createConnection();
-
-module.exports = client;
+module.exports = connection();
