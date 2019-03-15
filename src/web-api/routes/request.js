6e04c557f85fa802dcdc4bae7c0119a3b7ed7357
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-15 04:10:05 +0200

    Add logging incoming POST

--- src/web-api/routes/request.js
+++ src/web-api/routes/request.js
@@ -4,18 +4,23 @@ const koaBody = require('koa-body')({ multipart: true });
 const { WEB_API_V1_PREFIX, WEB_API_PATH_REQUEST: SUFFIX } = require('../../config');
 
 const route = path.join(WEB_API_V1_PREFIX, SUFFIX);
-
+// TODO: Limit max file size
+// TODO: Limit image types
 async function handlePost(ctx) {
-  const { files } = ctx.request;
-  console.log(files);
-  //   const { key, url } = await uploadFile({
-  //     fileName: file.name,
-  //     filePath: file.path,
-  //     fileType: file.type,
-  //   });
-  //   ctx.body = { key, url };
+  console.log('Fields: ', ctx.request.body);
+  console.log('Files: ', ctx.request.files);
+  ctx.body = JSON.stringify({ body: ctx.request.body, files: ctx.request.files }, null, 2);
 }
-
+/*
+ request.userName,
+ request.platformId,
+ request.platformType,
+ request.requestType,
+ request.longitude,
+ request.latitude,
+ request.photo,
+ request.message,
+*/
 module.exports = ({ router }) => {
   router.post(route, koaBody, async ctx => handlePost(ctx));
 };
