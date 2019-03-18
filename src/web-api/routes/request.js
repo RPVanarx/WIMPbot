6c863d63597356f549733bb4567fc649ddab4cfb
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-18 04:52:52 +0200

    Add request storing and better error handling

--- src/web-api/routes/request.js
+++ src/web-api/routes/request.js
@@ -1,47 +1,110 @@
 const path = require('path');
-const koaBody = require('koa-body')({ multipart: true });
+const { createRequest } = require('../../services');
 const validator = require('../utils/validator');
 
-const { WEB_API_V1_PREFIX, WEB_API_PATH_REQUEST: SUFFIX } = require('../../config');
+const {
+  WEB_API_V1_PREFIX,
+  WEB_API_PATH_REQUEST: SUFFIX,
+  PLATFORM_TYPE_TELEGRAM,
+  REQUEST_TYPE_SEARCH,
+  WEB_PHOTO_FILE_SIZE_MAX,
+  WEB_UPLOAD_TMP_DIR,
+} = require('../../config');
 
 const route = path.join(WEB_API_V1_PREFIX, SUFFIX);
 
-function validateFields(ctx) {
-  const result = validator.requestQuery(ctx.request.body);
-  ctx.assert(!result.length, 400, result.join(' '));
+function onKoaBodyError(err, ctx) {
+  ctx.assert(
+    !err.message && !err.message.includes('maxFileSize exceeded'),
+    400,
+    `File is too large! Must be less that ${WEB_PHOTO_FILE_SIZE_MAX} bytes!`,
+  );
+
+  ctx.throw(500, 'Cannot parse POST body', { error: err });
 }
 
-function validatePhoto(ctx) {
-  const result = validator.photoUpload(ctx.request.files);
-  ctx.assert(!result.length, 400, result.join(' '));
+// function onPhotoUlpoad(name, file) {
+// }
+
+// eslint-disable-next-line import/order
+const koaBody = require('koa-body')({
+  multipart: true,
+  onError: onKoaBodyError,
+  formidable: {
+    maxFileSize: WEB_PHOTO_FILE_SIZE_MAX,
+    uploadDir: WEB_UPLOAD_TMP_DIR,
+    // onFileBegin: onPhotoUlpoad,
+  },
+});
+
+function validatePost(ctx) {
+  let errors = [];
+  try {
+    errors = validator.requestQuery(ctx.request);
+  } catch (err) {
+    ctx.throw(500, 'POST body validation failed!', { error: err });
+  }
+
+  try {
+    errors = [...errors, ...validator.photoUpload(ctx.request)];
+  } catch (err) {
+    ctx.throw(500, 'POST file validation failed!', { error: err });
+  }
+  // TODO: remove photo if uploaded with errors
+  ctx.assert(!errors.length, 400, errors.join(' '));
 }
 
-function parseRequest(ctx) {
-  validateFields(ctx);
-  validatePhoto(ctx);
+async function getUser({ token }) {
+  // TODO: get user data
+  const { userId, userName } = { userId: 1, userName: 'Vasya' }//getOwner(token);
+  return { userId, userName };
 }
 
-// TODO: Limit max file size
-// TODO: Limit image types
+async function getPhotoId({ path }) {
+  // TODO: get photo id
+  // return //some fancy method to get id
+  return path;
+}
+
+function formRequest(body, userName, platformId, photo) {
+  return {
+    userName,
+    platformId,
+    platformType: PLATFORM_TYPE_TELEGRAM,
+    requestType: REQUEST_TYPE_SEARCH,
+    longitude: body.lon,
+    latitude: body.lat,
+    photo,
+    message: body.msg,
+  };
+}
+
 async function handlePost(ctx) {
-  parseRequest(ctx);
-  // get user key
-  // upload file and get id
-  // add request
-  console.log('Fields: ', ctx.request.body);
-  console.log('Files: ', ctx.request.files);
-  ctx.body = JSON.stringify({ body: ctx.request.body, files: ctx.request.files }, null, 2);
+  validatePost(ctx);
+
+  const { userName, userId } = getUser(ctx.request.body);
+
+  let photoId = null;
+  try {
+    photoId = await getPhotoId(ctx.request.files.photo);
+  } catch (err) {
+    ctx.throw(500, 'Server error: Cannot upload photo!', { error: err });
+  }
+
+  const request = formRequest(ctx.request.body, userName, userId, photoId);
+
+  let requestId;
+  try {
+    requestId = await createRequest(request);
+  } catch (err) {
+    ctx.throw(500, 'Cannot create request!', { error: err });
+  }
+
+  ctx.body = { request: requestId.toString() };
+
+  // ctx.body = JSON.stringify({ body: ctx.request.body, files: ctx.request.files }, null, 2);
 }
-/*
- request.userName = telegram user name,
- request.platformId = telegram user id,
- request.platformType = telegram,
- request.requestType = search,
- request.longitude,
- request.latitude,
- request.photo,
- request.message,
-*/
+
 module.exports = ({ router }) => {
   router.post(route, koaBody, async ctx => handlePost(ctx));
 };
