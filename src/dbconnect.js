8f872dd4f30f379b7c59af0fadc750e860fded4b
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-22 04:18:06 +0200

    Update dbconnect with pools and comment out init

--- src/dbconnect.js
+++ src/dbconnect.js
@@ -1,41 +1,53 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const { db } = require('./config');
-const migrate = require('./migrate');
+// const migrate = require('./migrate');
 const log = require('./logger')(__filename);
 
-const connection = () => {
-  return new Promise(async res => {
-    let client;
-    const dbInit = () => {
-      migrate.forEach(async req => {
-        await client.query(req);
-      });
-    };
-    let retries = db.RETRIES;
-    while (retries) {
-      try {
-        client = new Client({
-          user: db.USER,
-          host: db.HOST,
-          database: db.DATABASE,
-          password: db.PASSWORD,
-          port: db.PORT,
-        });
-        await client.connect();
-        log.info('database connected');
-        break;
-      } catch (error) {
-        retries -= 1;
-        if (!retries) {
-          process.exit(1);
-        }
-        log.error({ err: error.message }, `retries left ${retries}`);
-        await new Promise(resolve => setTimeout(resolve, db.DELAY));
-      }
-    }
-    await dbInit();
-    res(client);
-  });
-};
+const pool = new Pool({
+  user: db.USER,
+  host: db.HOST,
+  database: db.DATABASE,
+  password: db.PASSWORD,
+  port: db.PORT,
+});
 
-module.exports = connection();
+pool.on('error', error => {
+  log.error({ err: error.message }, 'DB: Uncaught pool error!');
+});
+/*
+async function init(dbPool, triesLeft) {
+  let client = null;
+  try {
+    client = await dbPool.connect();
+    log.info('DB init: connected - got client');
+  } catch (error) {
+    client.release();
+
+    log.error({ err: error.message }, `DB init: Failed to connect! Attempts left: ${triesLeft}`);
+    const tries = triesLeft - 1;
+
+    if (!tries) {
+      log.error({ err: error.message }, 'DB init: Unable to connect to database!');
+      process.exit(1);
+    }
+
+    setTimeout(() => init(dbPool, tries), db.DELAY);
+    return;
+  }
+
+  try {
+    if (!client) throw new Error('Cannot init! Client not ready!');
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const request of migrate) {
+      await client.query(request);
+    }
+  } catch (error) {
+    log.error({ err: error.message }, `DB init: Cannot init!`);
+  } finally {
+    client.release();
+  }
+}
+
+init(pool, db.RETRIES);
+*/
+module.exports = pool;
