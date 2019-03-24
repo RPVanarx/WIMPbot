0bf21b58d64798b22e81e9c78b0055fa21ca1bb8
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-24 20:58:51 +0200

    Update dbconnect to use dbinit logic

--- src/dbconnect.js
+++ src/dbconnect.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
+const dbinit = require('./dbinit');
 const { db } = require('./config');
-const migrate = require('./migrate');
 const log = require('./logger')(__filename);
+
+if (!dbinit.isInitialized) {
+  log.warn(`DBconnect: DB is not initialized! You should run init first!`);
+}
 
 const pool = new Pool({
   user: db.USER,
@@ -12,48 +16,7 @@ const pool = new Pool({
 });
 
 pool.on('error', error => {
-  log.error({ err: error.message }, 'DB: Uncaught pool error!');
+  log.error({ err: error.message }, 'DBconnect: Uncaught pool error!');
 });
-
-async function init(dbPool, triesLeft) {
-  let client = null;
-  try {
-    client = await dbPool.connect();
-    log.info('DB init: connected - got client');
-  } catch (error) {
-    if (client) client.release();
-
-    log.error({ err: error.message }, `DB init: Failed to connect! Attempts left: ${triesLeft}`);
-    const tries = triesLeft - 1;
-
-    if (!tries) {
-      log.error({ err: error.message }, 'DB init: Unable to connect to database!');
-      process.exit(1);
-    }
-    setTimeout(() => init(dbPool, tries), db.DELAY);
-    return;
-  }
-
-  if (!client) {
-    const err = new Error('DB init: Cannot init! DB client not ready!');
-    log.error({ err: err.message }, err.message);
-    process.exit(1);
-  }
-  // eslint-disable-next-line no-restricted-syntax
-  for await (const request of migrate) {
-    try {
-      await client.query(request);
-    } catch (error) {
-      if (client) client.release();
-
-      log.error({ err: error.message }, `DB init: Cannot execute request: ${request}`);
-      process.exit(1);
-    }
-  }
-
-  if (client) client.release();
-}
-
-init(pool, db.RETRIES);
 
 module.exports = pool;
