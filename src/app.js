2d88477e14e77e5d8e339fab478b53b7d186197a
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-26 00:40:56 +0200

    Move App.js requires into start function
    ...to prevent module loading before init process has been finished

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const dbinit = require('./dbinit');
-const telegram = require('./telegram');
-const api = require('./testapi/api');
-const webApiServer = require('./web-api');
 
 function start() {
+  const telegram = require('./telegram'); // eslint-disable-line global-require
+  const api = require('./testapi/api'); // eslint-disable-line global-require
+  const webApiServer = require('./web-api'); // eslint-disable-line global-require
+
   telegram.launch();
   api.listen();
   webApiServer.listen();
