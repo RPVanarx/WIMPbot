71a27b18413e7f6e6517d5b4a1ff2923a150704c
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-25 22:03:29 +0200

    Add web-token logic

--- src/web-api/utils/web-token.js
+++ src/web-api/utils/web-token.js
@@ -1,14 +1,57 @@
-const validator = require('./validator');
+const crypto = require('crypto');
+const { WEB_TOKEN_KEY } = require('../../config');
 
-function getUserCredentials({ webToken }) {
-  const errors = validator.webToken({ token: webToken });
+const algorithm = 'aes-256-cbc';
 
-  if (errors.length) throw new Error(errors.join(' '));
+const ivSize = 16; // aes-256 = 256b = 16B
+const idMaxLength = 64; // 18 - Telegram's max
 
-  const { userId, userName } = { userId: '1', userName: 'decryptor' };//decrypt(webToken) TODO: decrypt webToken
-  return { userId, userName };
+function getKey() {
+  const hash = crypto.createHash('sha256');
+  hash.update(WEB_TOKEN_KEY.toString());
+  return hash.digest();
+}
+
+function encrypt(decryptedToken) {
+  const iv = crypto.randomBytes(ivSize);
+
+  const cipher = crypto.createCipheriv(algorithm, getKey(), iv);
+  const dataBuf = cipher.update(decryptedToken, 'utf8');
+  const finalBuf = cipher.final();
+
+  const encrypted = Buffer.concat([dataBuf, finalBuf, iv]);
+  return encrypted.toString('base64');
+}
+
+function decrypt(encryptedToken) {
+  const encrypted = Buffer.from(encryptedToken, 'base64');
+
+  const iv = encrypted.slice(encrypted.length - ivSize);
+  const encryptedData = encrypted.slice(0, encrypted.length - ivSize);
+
+  const decipher = crypto.createDecipheriv(algorithm, getKey(), iv);
+  return decipher.update(encryptedData, 'utf8') + decipher.final('utf8');
+}
+
+function createToken({ id, date }) {
+  if (typeof id !== 'string') {
+    throw new TypeError('WEB-token: ID must be a string!');
+  }
+  if (id.length > idMaxLength) {
+    throw new Error(`WEB-token: ID is too long! Max: ${idMaxLength} characters`);
+  }
+
+  const decryptedToken = JSON.stringify({ id, date });
+  return encrypt(decryptedToken);
+}
+
+function getUserCredentials(webToken) {
+  const decryptedToken = decrypt(webToken);
+  const token = JSON.parse(decryptedToken);
+  return token;
 }
 
 module.exports = {
   getUserCredentials,
+  createToken,
 };
