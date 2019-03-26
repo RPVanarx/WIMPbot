7f56103c45f160d07bce3229b5c94a07e82000c3
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-26 20:53:21 +0200

    Add telegram authentication to web-api/utils/index

--- src/web-api/utils/index.js
+++ src/web-api/utils/index.js
@@ -2,10 +2,12 @@ const errorResponse = require('./error-handling');
 const photo = require('./photo');
 const validator = require('./validator');
 const webToken = require('./web-token');
+const authTelegram = require('./telegram-authorization');
 
 module.exports = {
   errorResponse,
   photo,
   validator,
   webToken,
+  authTelegram,
 };
