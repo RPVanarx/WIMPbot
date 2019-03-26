72ad94c76b1cce3cc58c46a82382a99305b1fc43
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-26 19:15:45 +0200

    Update web-api validator with global rules

--- src/web-api/utils/validator.js
+++ src/web-api/utils/validator.js
@@ -1,77 +1,24 @@
 const { Validator, Rule } = require('@cesium133/forgjs');
-const { DEFAULT_VALUES } = require('../../config');
+const globalValidator = require('../../utils/global-validator');
 
-// HACK: Valid for 1.1.8 forgjs
-// TODO: Remove next block when
-// https://github.com/oussamahamdaoui/forgJs/issues/65
-// is fixed
-function removeBugsDecorator(func) {
-  // eslint-disable-next-line func-names
-  return function(...args) {
-    // Removes duplicates from array
-    return [...new Set(func.apply(this, args))];
-  };
-}
-Validator.prototype.getErrors = removeBugsDecorator(Validator.prototype.getErrors);
-// block end -----
-
-const location = new Validator({
-  lon: new Rule(
+const photoUploadValidator = new Validator({
+  type: new Rule(
     {
-      type: 'string-float|string-int',
-      min: DEFAULT_VALUES.LONGITUDE_MIN,
-      max: DEFAULT_VALUES.LONGITUDE_MAX,
+      type: 'string',
+      match: /^image\/.+/,
     },
-    `Longitude must be a number in range ${DEFAULT_VALUES.LONGITUDE_MIN} to ${
-      DEFAULT_VALUES.LONGITUDE_MAX
-    }!`,
+    `Photo must be of type 'image/*'!`,
   ),
-  lat: new Rule(
+  fieldName: new Rule(
     {
-      type: 'string-float|string-int',
-      min: DEFAULT_VALUES.LATITUDE_MIN,
-      max: DEFAULT_VALUES.LATITUDE_MAX,
+      type: 'string',
+      equal: 'photo',
     },
-    `Latitude must be a number in range ${DEFAULT_VALUES.LATITUDE_MIN} to ${
-      DEFAULT_VALUES.LATITUDE_MAX
-    }!`,
+    'Unexpected field name of file!',
   ),
 });
 
-const daysAndRadius = new Validator({
-  r: new Rule(
-    { type: 'string-int', min: DEFAULT_VALUES.RADIUS_MIN, max: DEFAULT_VALUES.RADIUS_MAX },
-    `Radius must be an integer in range ${DEFAULT_VALUES.RADIUS_MIN} to ${
-      DEFAULT_VALUES.RADIUS_MAX
-    }!`,
-  ),
-  d: new Rule(
-    { type: 'string-int', min: DEFAULT_VALUES.DAYS_MIN, max: DEFAULT_VALUES.DAYS_MAX },
-    `Days must be an integer in range ${DEFAULT_VALUES.DAYS_MIN} to ${DEFAULT_VALUES.DAYS_MAX}!`,
-  ),
-});
-
-const photoUpload = new Validator({
-  type: new Rule({ type: 'string', match: /^image\/.+/ }, `Photo must be of type 'image/*'!`),
-  fieldName: new Rule({ type: 'string', equal: 'photo' }, 'Unexpected field name of file!'),
-});
-
-const requestFieldsOptional = new Validator({
-  msg: new Rule(
-    { type: 'string', minLength: 1, maxLength: DEFAULT_VALUES.REQUEST_MESSAGE_MAX, optional: true },
-    `Message must not be empty, must not exceed ${DEFAULT_VALUES.REQUEST_MESSAGE_MAX} symbols!`,
-  ),
-  lon: new Rule(
-    { type: 'string-float|string-int', min: -180, max: 180, optional: true },
-    'Longitude must be a number in range -180 to 180!',
-  ),
-  lat: new Rule(
-    { type: 'string-float|string-int', min: -90, max: 90, optional: true },
-    'Latitude must be a number in range -90 to 90!',
-  ),
-});
-
-const requestFieldsPresense = new Validator({
+const requestFieldsPresenсeValidator = new Validator({
   lon: new Rule({ type: 'string' }, `'longitude' field not found!`),
   lat: new Rule({ type: 'string' }, `'latitude' field not found!`),
   msg: new Rule({ type: 'string' }, `'message' field not found!`),
@@ -80,26 +27,39 @@ const requestFieldsPresense = new Validator({
 
 module.exports = {
   listQuery({ d, r, lon, lat }) {
-    return [...location.getErrors({ lon, lat }), ...daysAndRadius.getErrors({ d, r })];
+    return [
+      ...globalValidator.location({ lon, lat }),
+      ...globalValidator.daysAndRadius({ days: d, radius: r }),
+    ];
   },
 
   signupQuery({ lon, lat }) {
-    return [...location.getErrors({ lon, lat })];
+    return [...globalValidator.location({ lon, lat })];
   },
 
-  requestFieldsOptional({ msg, lon, lat, token }) {
-    return requestFieldsOptional.getErrors({ msg, lon, lat, token });
+  requestFormFieldsOptional({ msg, lon, lat }) {
+    let errors = [];
+    if (msg != null) {
+      errors = [...errors, globalValidator.messageRequest({ message: msg })];
+    }
+    if (lon != null) {
+      errors = [...errors, globalValidator.longitude({ lon })];
+    }
+    if (lat != null) {
+      errors = [...errors, globalValidator.latitude({ lat })];
+    }
+    return errors;
   },
 
-  photoUpload({ fieldName, type }) {
-    return photoUpload.getErrors({ fieldName, type });
-  },
-
-  requestFormData({ photoUploadPromise, msg, lon, lat, token }) {
+  requestFormPresence({ photoUploadPromise, msg, lon, lat, token }) {
     const errors = [];
     if (!(photoUploadPromise instanceof Promise)) {
       errors.push('No photo provided!');
     }
-    return [...errors, ...requestFieldsPresense.getErrors({ msg, lon, lat, token })];
+    return [...errors, ...requestFieldsPresenсeValidator.getErrors({ msg, lon, lat, token })];
+  },
+
+  photoUpload({ fieldName, type }) {
+    return photoUploadValidator.getErrors({ fieldName, type });
   },
 };
