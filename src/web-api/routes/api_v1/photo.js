a256adfd81a174d0838a76fdcf5d0dac32c5c638
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-30 22:01:42 +0200

    Fix photo route

---
+++ src/web-api/routes/api_v1/photo.js
@@ -0,0 +1,26 @@
+const Router = require('koa-router');
+const photo = require('../../../utils/photo');
+const { WEB_API_PATH_PHOTO } = require('../../../config');
+
+const router = new Router({
+  prefix: WEB_API_PATH_PHOTO,
+});
+
+async function getPhoto(ctx) {
+  ctx.assert(ctx.accepts('image/*'), 415, 'Client is not able to accept images!');
+
+  const photoId = ctx.params.id;
+
+  try {
+    ctx.body = await photo.getPhotoStream(photoId);
+    ctx.type = 'image/jpeg';
+  } catch (err) {
+    if (err.code === 404) ctx.throw(404, 'Photo not found', { error: err });
+
+    ctx.throw(500, 'Cannot get photo', { error: err });
+  }
+}
+
+router.get('/:id', getPhoto);
+
+module.exports = router;
