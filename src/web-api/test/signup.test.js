d69a00deb8703a9c6f31b40802f0b4d2442c3c2a
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-30 22:46:28 +0200

    Fix signup test

--- src/web-api/test/signup.test.js
+++ src/web-api/test/signup.test.js
@@ -7,10 +7,14 @@ const server = koaApp.callback();
 const route = `${WEB_API_V1_PREFIX}${WEB_API_PATH_SIGNUP}`;
 
 describe('/signup route test', () => {
-  describe('JSON test', () => {
-    const fakeToken = webToken.create('0');
-    const validFakeRequest = `${route}?lon=2&lat=3`;
+  let fakeToken;
+  let validFakeRequest;
+  beforeAll(() => {
+    fakeToken = webToken.create('0');
+    validFakeRequest = `${route}?lon=2&lat=3`;
+  });
 
+  describe('JSON test', () => {
     test(`should response with status 200 and proper JSON on valid request`, async () => {
       const response = await request(server)
         .get(validFakeRequest)
@@ -26,7 +30,9 @@ describe('/signup route test', () => {
 
   describe('Error test', () => {
     test(`should response with status 400 on empty query GET ${route}`, async () => {
-      const response = await request(server).get(route);
+      const response = await request(server)
+        .get(route)
+        .set('Cookie', [`token=${fakeToken}`]);
       expect(response.status).toEqual(400);
     });
     test(`should response with JSON that contains error message`, async () => {
@@ -38,11 +44,9 @@ describe('/signup route test', () => {
       expect(json.error).toBeTruthy();
     });
     test(`should response with status 400 on invalid query`, async () => {
-      let response = await request(server).get(`${route}?a=1&b=2`);
-      expect(response.status).toEqual(400);
-      response = await request(server).get(`${route}?a`);
-      expect(response.status).toEqual(400);
-      response = await request(server).get(`${route}?`);
+      const response = await request(server)
+        .get(`${route}?a=1&b=2`)
+        .set('Cookie', [`token=${fakeToken}`]);
       expect(response.status).toEqual(400);
     });
     test(`should response with status 401 on invalid token`, async () => {
