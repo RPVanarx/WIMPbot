738076509c0020e64515df606150a23e635a6e7e
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-30 21:05:28 +0200

    Refactor web-api index

--- src/web-api/index.js
+++ src/web-api/index.js
@@ -1,46 +1,21 @@
 const Koa = require('koa');
-const Router = require('koa-router');
-const { WEB_PORT } = require('../config');
-const { set404, handleError } = require('./utils/error-handling');
+
 const log = require('../logger')(__filename);
+const errorHandler = require('./utils/error-handler');
+const rootRouter = require('./routes');
+
+const { WEB_PORT } = require('../config');
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  try {
-    await next();
-
-    if (ctx.status === 404) set404(ctx);
-  } catch (err) {
-    if (handleError(err, ctx)) return;
-
-    ctx.status = err.status || 500;
-    ctx.body = !err.error ? 'Internal server error' : err.message;
-
-    ctx.app.emit('error', err, ctx);
-  }
-});
-
 app.on('error', (err, ctx) => {
-  // console.error('------ !!! ERRORR !!! ------');
-  log.error({ err }, `Path: ${ctx.path}`);
+  log.error({ err }, `WEB: HREF: ${ctx.href}`);
 });
 
-function createRouter(route, KoaRouter = Router, koaApp = app) {
-  const router = new KoaRouter();
-  route({ router });
+app.use(errorHandler());
 
-  koaApp.use(router.routes());
-  koaApp.use(router.allowedMethods());
-
-  return router;
-}
-createRouter(require('./routes/root'));
-createRouter(require('./routes/photo'));
-createRouter(require('./routes/requests'));
-createRouter(require('./routes/request'));
-createRouter(require('./routes/signin'));
-createRouter(require('./routes/signup'));
+app.use(rootRouter.routes());
+app.use(rootRouter.allowedMethods());
 
 function listen() {
   return app.listen(WEB_PORT, () => {
