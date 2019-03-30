8375637f942a83081e4f31456e908dd5221112aa
Vitali Timolyanov <nf.ebox@gmail.com>
2019-03-30 21:08:02 +0200

    Update root test

--- src/web-api/test/root.test.js
+++ src/web-api/test/root.test.js
@@ -4,24 +4,45 @@ const { koaApp } = require('../index.js');
 const server = koaApp.callback();
 
 describe('root route test', () => {
-  test('should response with body and status 200 on GET /', async () => {
-    const response = await request(server).get('/');
-    expect(response.status).toEqual(200);
-    expect(response.text).toBeTruthy();
+  describe('Success:', () => {
+    test('should response with JSON on GET /', async () => {
+      const response = await request(server).get('/');
+      expect(response.status).toEqual(200);
+      expect(response.text).toBeTruthy();
+      expect(response.headers['content-type']).toContain('application/json');
+
+      const json = JSON.parse(response.text);
+      expect(json).toHaveProperty('usage');
+      expect(json.usage).toBeTruthy();
+    });
   });
-  test('should response with body and status 200 on GET /api', async () => {
-    const response = await request(server).get('/api');
-    expect(response.status).toEqual(200);
-    expect(response.text).toBeTruthy();
-  });
-  test('should response with body and status 200 on GET /api/v1', async () => {
-    const response = await request(server).get('/api/v1');
-    expect(response.status).toEqual(200);
-    expect(response.text).toBeTruthy();
-  });
-  test('should response with body and status 404 on GET /123/abc', async () => {
-    const response = await request(server).get('/123/abc');
-    expect(response.status).toEqual(404);
-    expect(response.text).toBeTruthy();
+  describe('Errors:', () => {
+    test('should response with status 404 on GET /aaa', async () => {
+      const response = await request(server).get('/aaa');
+      expect(response.status).toEqual(404);
+      expect(response.text).toBeTruthy();
+    });
+    test('should response with JSON error on GET /abc', async () => {
+      const response = await request(server).get('/abc');
+      expect(response.status).toEqual(404);
+      expect(response.text).toBeTruthy();
+      expect(response.headers['content-type']).toContain('application/json');
+
+      const json = JSON.parse(response.text);
+      expect(json).toHaveProperty('error');
+      expect(json.error).toBeTruthy();
+    });
+    test('should response with text and status 404 on GET /qwe', async () => {
+      const response = await request(server)
+        .get('/qwe')
+        .accept('text/html');
+      expect(response.status).toEqual(404);
+      expect(response.text).toBeTruthy();
+    });
+    test('should response with body and status 404 on GET /123/abc', async () => {
+      const response = await request(server).get('/123/abc');
+      expect(response.status).toEqual(404);
+      expect(response.text).toBeTruthy();
+    });
   });
 });
