6540a0b255502045b398ef74cc5a279690e4e4b9
Vitali Timolyanov <nf.ebox@gmail.com>
2019-04-06 03:43:45 +0300

    Add CORS to web-api

--- src/web-api/index.js
+++ src/web-api/index.js
@@ -1,16 +1,27 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 
 const log = require('../logger')(__filename);
 const errorHandler = require('./utils/error-handler');
 const rootRouter = require('./routes');
 
-const { WEB_PORT } = require('../config');
+const { WEB_PORT, WEB_CORS_ORIGIN } = require('../config');
 
 const app = new Koa();
 
 app.on('error', (err, ctx) => {
   log.error({ err }, `WEB: HREF: ${ctx.href}`);
 });
+
+const corsOptions = {
+  origin: WEB_CORS_ORIGIN,
+  allowMethods: 'GET,POST,OPTIONS',
+  maxAge: 86400,
+  credentials: true,
+  keepHeadersOnError: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(errorHandler());
 
