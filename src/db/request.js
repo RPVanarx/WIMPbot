3b30f8659943fca2da6567edd2ce444042135db3
Vitali Timolyanov <nf.ebox@gmail.com>
2019-05-03 15:38:03 +0300

    Update DB wrappers with knex

--- src/db/request.js
+++ src/db/request.js
@@ -1,97 +1,63 @@
-const client = require('../dbconnect');
+const knex = require('./knex');
 
-async function create(request) {
-  await client.query(
-    `UPDATE users SET user_name = $1, bad_request_count = bad_request_count + 1 
-    WHERE platform_id = $2 AND platform_type = $3`,
-    [request.userName, request.platformId, request.platformType],
-  );
+const { distance, knexHelper: helper } = require('../utils');
 
-  return (await client.query(
-    `INSERT INTO requests VALUES(
-        DEFAULT,
-        (SELECT id FROM users WHERE platform_id = $1 AND platform_type = $2), 
-        $3, 
-        (point($4, $5)), 
-        $6, 
-        $7, 
-        now()) 
-        RETURNING id`,
-    [
-      request.platformId,
-      request.platformType,
-      request.requestType,
-      request.longitude,
-      request.latitude,
-      request.photo,
-      request.message,
-    ],
-  )).rows[0].id;
-}
-
-async function getRequestsToDelete({ platformId, platformType }) {
-  return (await client.query(
-    `SELECT * FROM requests 
-    WHERE user_id = (SELECT id FROM users WHERE platform_id = $1 AND platform_type = $2) 
-    AND is_active = true`,
-    [platformId, platformType],
-  )).rows;
-}
-
-async function deleteRequest(id) {
-  await client.query('UPDATE requests SET is_active = false WHERE id = $1', [id]);
-  return true;
-}
-
-async function search({ platformId, platformType, radius, days }) {
-  return (await client.query(
-    `SELECT r.id, r.request_type, r.photo, r.message, r.creation_date, r.location, u.user_name, u.platform_type 
-    FROM requests AS r, users AS u 
-    WHERE ((SELECT location FROM users WHERE platform_id = $1 AND platform_type = $2) <@> r.location) <= $3/1609.34 
-    AND u.id = r.user_id 
-    AND r.is_active = true 
-    AND r.creation_date >= (now() AT TIME ZONE 'UTC' - $4 * interval '1 day')`,
-    [platformId, platformType, radius, days],
-  )).rows;
-}
-
-async function searchInArea({ longitude, latitude, radius, days }) {
-  return (await client.query(
-    `SELECT r.id, r.request_type, r.photo, r.message, r.creation_date, r.location, u.user_name, u.platform_type 
-    FROM requests AS r, users AS u 
-    WHERE (point($1, $2) <@> r.location) <= $3/1609.34 
-    AND u.id = r.user_id 
-    AND r.is_active = true 
-    AND r.creation_date >= (now() AT TIME ZONE 'UTC' - $4 * interval '1 day')`,
-    [longitude, latitude, radius, days],
-  )).rows;
-}
-
-async function changeActiveStatus({ reqId, status, moderatorId }) {
-  const request = (await client.query(
-    `UPDATE requests SET is_approved = $2, is_active = $2, status_changed_by = $3 
-    WHERE id = $1 RETURNING *`,
-    [reqId, status, moderatorId],
-  )).rows[0];
-  const userReq = (await client.query(
-    `SELECT user_name, platform_type, platform_id FROM users 
-    WHERE id = $1`,
-    [request.user_id],
-  )).rows[0];
-  if (!status) return Object.assign(request, userReq);
-  await client.query(
-    `UPDATE users SET bad_request_count = bad_request_count - 1 
-    WHERE id = $1`,
-    [request.user_id],
-  );
-  return Object.assign(request, userReq);
-}
+const REQUESTS = 'requests';
 
 module.exports = {
-  create,
-  getRequestsToDelete,
-  deleteRequest,
-  search,
-  searchInArea,
-  changeActiveStatus,
+  async create({ userId, requestType, longitude, latitude, photo, message }) {
+    const results = await knex(REQUESTS)
+      .insert({
+        userId,
+        requestType,
+        location: helper.toPoint({ longitude, latitude }),
+        photo,
+        message,
+      })
+      .returning(['id', 'created']);
+    return results[0];
+  },
+
+  update({ id, active, approved, moderatedBy }) {
+    return knex(REQUESTS)
+      .update({ active, approved, moderatedBy })
+      .where({ id })
+      .returning('id');
+  },
+
+  get(id) {
+    return knex(REQUESTS)
+      .where({ id })
+      .first();
+  },
+
+  getLastRequest({ userId }) {
+    return knex(REQUESTS)
+      .where({ userId })
+      .orderBy('created', 'desc')
+      .first();
+  },
+
+  getUserRequests(ownerId) {
+    return knex(REQUESTS).where({ userId: ownerId, active: true });
+  },
+
+  getInArea({ location, radius, days }) {
+    const radiusMi = distance.metersToMiles(radius);
+    return knex({ r: REQUESTS })
+      .join('users as u', { 'r.userId': 'u.id' })
+      .where({ 'r.active': true })
+      .andWhereRaw('r.location <@> point(?, ?) <= ?', [location.x, location.y, radiusMi])
+      .andWhereRaw(`r.created >= (now() AT TIME ZONE 'UTC' - ? * interval '1 day')`, [days])
+      .select(
+        'r.id',
+        'r.requestType',
+        'r.photo',
+        'r.message',
+        'r.created',
+        'r.location',
+        'u.username',
+        'u.platformType',
+      );
+  },
 };
--- src/db/user.js
+++ src/db/user.js
@@ -1,150 +1,66 @@
-const client = require('../dbconnect');
+const knex = require('./knex');
+
+const { distance, knexHelper: helper } = require('../utils');
+
 const {
   defaultValues: { RADIUS },
 } = require('../config');
 
-async function create({ platformId, platformType, longitude, latitude }) {
-  await client.query(
-    'INSERT INTO users VALUES(DEFAULT, $1, $2, DEFAULT, (point($3, $4))) ON CONFLICT (platform_id, platform_type) DO UPDATE SET location = (point($3, $4))',
-    [platformId, platformType, longitude, latitude],
-  );
-  return true;
-}
-
-async function changeActivity({ platformId, platformType, value }) {
-  await client.query(
-    'UPDATE users SET is_active = $1 WHERE platform_id = $2 AND platform_type = $3',
-    [value, platformId, platformType],
-  );
-  return true;
-}
-
-async function getActivityStatus({ platformId, platformType }) {
-  return (await client.query(
-    'SELECT is_active FROM users WHERE platform_id = $1 AND platform_type = $2',
-    [platformId, platformType],
-  )).rows[0].is_active;
-}
-
-async function findUsersInRequestRadius(location) {
-  return (await client.query(
-    `SELECT platform_id, platform_type FROM users 
-    WHERE is_active = true 
-    AND location <@> point($1, $2) <= $3/1609.34`,
-    [location.x, location.y, RADIUS],
-  )).rows;
-}
-
-async function badRequestCount({ platformId, platformType }) {
-  return (await client.query(
-    'SELECT bad_request_count FROM users WHERE platform_id = $1 and platform_type = $2',
-    [platformId, platformType],
-  )).rows[0].bad_request_count;
-}
-
-async function getTimeOfLastRequest({ platformId, platformType }) {
-  return (await client.query(
-    `SELECT creation_date FROM requests 
-    WHERE user_id = (SELECT id FROM users WHERE platform_id = $1 and platform_type = $2)
-    ORDER BY creation_date DESC`,
-    [platformId, platformType],
-  )).rows[0].creation_date;
-}
-
-async function resetBadRequest({ platformId, platformType }) {
-  await client.query(
-    `UPDATE users SET bad_request_count = 0 
-    WHERE platform_id = $1 and platform_type = $2`,
-    [platformId, platformType],
-  );
-  return true;
-}
-
-async function getId({ platformId, platformType }) {
-  const response = await client.query(
-    `SELECT id FROM users
-    WHERE platform_id = $1 AND platform_type = $2`,
-    [platformId, platformType],
-  );
-
-  if (!response.rows.length) return undefined;
-
-  return response.rows[0].id;
-}
-
-async function getName({ platformId, platformType }) {
-  const response = await client.query(
-    `SELECT user_name FROM users
-        WHERE platform_id = $1 AND platform_type = $2`,
-    [platformId, platformType],
-  );
-  return response.rows[0].user_name;
-}
-
-async function setName({ platformId, platformType, userName }) {
-  await client.query(
-    `UPDATE users SET user_name = $3 
-        WHERE platform_id = $1 and platform_type = $2`,
-    [platformId, platformType, userName],
-  );
-  return true;
-}
-
-async function getPlatformId(requestId) {
-  return (await client.query(
-    `SELECT platform_id FROM users 
-      WHERE id = (SELECT user_id FROM requests WHERE id = $1)`,
-    [requestId],
-  )).rows[0].platform_id;
-}
-
-async function getPlatformTypeFromRequest(requestId) {
-  return (await client.query(
-    `SELECT platform_type FROM users 
-          WHERE id = (SELECT user_id FROM requests WHERE id = $1)`,
-    [requestId],
-  )).rows[0].platform_type;
-}
-
-async function getStep({ platformId, platformType }) {
-  return (await client.query(
-    `SELECT step FROM users 
-    WHERE platform_id = $1 and platform_type = $2`,
-    [platformId, platformType],
-  )).rows[0].step;
-}
-
-async function setStep({ platformId, platformType, value }) {
-  await client.query(
-    `UPDATE users SET step = $1  
-        WHERE platform_id = $2 and platform_type = $3`,
-    [value, platformId, platformType],
-  );
-  return true;
-}
-
-async function getLocation({ platformId, platformType }) {
-  return (await client.query(
-    `SELECT location FROM users 
-        WHERE platform_id = $1 and platform_type = $2`,
-    [platformId, platformType],
-  )).rows[0].location;
-}
+const USERS = 'users';
+const RADIUS_MI = distance.metersToMiles(RADIUS);
 
 module.exports = {
-  create,
-  changeActivity,
-  getActivityStatus,
-  findUsersInRequestRadius,
-  badRequestCount,
-  getTimeOfLastRequest,
-  resetBadRequest,
-  getId,
-  getName,
-  setName,
-  getPlatformId,
-  getPlatformTypeFromRequest,
-  getStep,
-  setStep,
-  getLocation,
+  async create({ platformId, platformType, longitude, latitude }) {
+    const ids = await knex(USERS)
+      .insert({
+        platformId,
+        platformType,
+        location: helper.toPoint({ longitude, latitude }),
+      })
+      .returning('id');
+    return ids[0];
+  },
+
+  get({ id, platformId, platformType }) {
+    return knex(USERS)
+      .where(builder => {
+        if (id) return builder.where({ id });
+
+        return builder.where({ platformId, platformType });
+      })
+      .first();
+  },
+
+  update({
+    id,
+    platformId,
+    platformType,
+    username,
+    longitude,
+    latitude,
+    badRequests,
+    active,
+    step,
+  }) {
+    return knex(USERS)
+      .where(builder => {
+        if (id) return builder.where({ id });
+
+        return builder.where({ platformId, platformType });
+      })
+      .update({
+        username,
+        location: helper.toPoint({ longitude, latitude }),
+        badRequests,
+        active,
+        step,
+      })
+      .returning('id');
+  },
+
+  async getUsersInRadius(location) {
+    return knex(USERS)
+      .where({ active: true })
+      .andWhereRaw('location <@> point(?, ?) <= ?', [location.x, location.y, RADIUS_MI]);
+  },
 };
